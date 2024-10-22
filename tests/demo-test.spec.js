be25d3b15df74d18078902ef0e4dc59e038ad1ac
Bekzod <begzod0320@gmail.com>
2024-10-22 15:31:41 -0500

    initial commit

---
+++ tests/demo-test.spec.js
@@ -0,0 +1,29 @@
+import {test, expect} from '@playwright/test'
+
+test('Testing Login Page', async ({page}) => {
+
+    await page.goto('https://demo.applitools.com/');
+    await page.pause()
+
+})
+
+
+test.only('Assertion test demo', async({ page }) => {
+
+    await page.goto('https://kitchen.applitools.com/')
+    await page.pause()
+
+    // ASSERT
+
+    // Checking element presence
+    await expect(page.getByRole('heading', { name: 'The Kitchen' })).toHaveCount(1)
+
+    // Confirm URl
+    await expect(page).toHaveURL('https://kitchen.applitools.com/')
+    await expect(page).toHaveTitle('The Kitchen')
+
+    // Visual validation with screenshot
+
+    await expect(page).toHaveScreenshot()
+    
+})
